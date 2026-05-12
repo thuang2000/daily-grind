d5f642e8007f4fcfba32f14051d3dc834d7a07a3
thuang2000 <thuangnsc@gmail.com>
2026-05-11 23:16:25 -0700

    Tuesday working?

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -47,5 +47,21 @@ switch(myDay){
 }
 
 console.log(coffee);
-alert(today);
+alert(coffeeTemplate(coffee));
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+
+function coffeeTemplate(coffee){
+    let myReturn = "";
+
+    myReturn += `
+        <p>
+<img src="images/${coffee.pic}" alt="${coffee.alt}" id="coffee">
+<strong>${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong>${coffee.name}</strong> ${coffee.desc}
+</p>
+
+    `;
+
+    return myReturn;
+}
+
 
